935e4e0c71de931754eb6b76864ba8389ab92232
Sneha Panjwani <snehapanjwani26@gmail.com>
2025-10-23 14:22:05 +0530

    update

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ import mongoose from "mongoose";
 import express from "express";
 import path from "path";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 import router from "./routers/WebRouter.js";
 import admin from "./routers/AdminRouter.js";
 import fileUpload from "express-fileupload";
 import authRouter from "./routers/AuthRouter.js";
 
-let conn = await mongoose.connect("mongodb://localhost:27017/kidsearthdb");
+let conn = await mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+console.log("MongoDB connected:", conn.connection.host);
 
 const app = express();
 app.use(cors());
