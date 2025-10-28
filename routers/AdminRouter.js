da5abb42553a1324208292a9741749fb9679e577
Sneha Panjwani <snehapanjwani26@gmail.com>
2025-10-28 16:57:34 +0530

    changes

--- routers/AdminRouter.js
+++ routers/AdminRouter.js
@@ -6,8 +6,7 @@ import validator from "validator";
 import { Admin } from "../models/Admin.js";
 import { Product } from "../models/Product.js";
 import path from "path";
-import { v4 as uuidv4 } from "uuid";
-
+import {v4 as uuidv4} from "uuid";
 
 
 const router = Router();
