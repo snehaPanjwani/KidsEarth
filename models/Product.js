0fb852926a6a125cba7f662bd9e8f990723000cd
Sneha Panjwani <snehapanjwani26@gmail.com>
2025-10-28 20:11:52 +0530

    Add size field to Product model

--- models/Product.js
+++ models/Product.js
@@ -8,6 +8,7 @@ const ProductSchema = new mongoose.Schema({
    discount: { type: Number, required: true },
    description: { type: String, required: true },
    category: { type: String, required: true },
+   size: { type: [String], required: true },
    stock: { type: Number, required: true },
    quantity : { type:Number, required:true},
    image: { type: String, required: true },
